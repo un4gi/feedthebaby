0f584deffdd9bb40af312faafa9924e6dad45850
Tony West <47873286+un4gi@users.noreply.github.com>
2022-08-21 14:24:58 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	BOT_TOKEN  string = "DISCORD_BOT_TOKEN_HERE"
+	CHANNEL_ID string = "DISCORD_CHANNEL_ID_HERE"
+	USER_AGENT string = "github.com/un4gi/feedthebaby"
+)
+
+func main() {
+	CheckTargetItems()
+}
